dd4b6f0b2ca2ac0f9fdbb40842e146323be8b1c3
Uman <mkumar2@gmail.com>
2018-08-25 21:31:59 +0530

    first commit pushing project.

---
+++ UpsTest/src/main/java/com/ims/ups/AppTest.java
@@ -0,0 +1,14 @@
+package com.ims.ups;
+
+/**
+ * Hello world!
+ *
+ */
+public class AppTest 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World" );
+        System.out.println("hi");
+    }
+}
